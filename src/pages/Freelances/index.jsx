ddd0dc87a08ee9e52e795699740114e4f71d8c1a
Eric Faraut <eric.faraut@hotmail.fr>
2025-03-30 21:02:53 +0200

    Types are secured with prop-types.

--- src/pages/Freelances/index.jsx
+++ src/pages/Freelances/index.jsx
@@ -1,7 +1,36 @@
+import DefaultPicture from "../../assets/photo.png";
+import Card from "../../components/Card/";
+
+const freelanceProfiles = [
+    {
+        name: "Jane Doe",
+        jobTitle: "DevOps",
+        picture: DefaultPicture,
+    },
+    {
+        name: "John Doe",
+        jobTitle: "Développeur frontend",
+        picture: DefaultPicture,
+    },
+    {
+        name: "Jeanne Biche",
+        jobTitle: "Développeuse fullstack",
+        picture: DefaultPicture,
+    },
+];
+
 function Freelances() {
     return (
         <div>
-            <h1>Freelances</h1>
+            <h1>Freelances 👩·💻👨·💻👩·💻</h1>
+            {freelanceProfiles.map((profile, index) => (
+                <Card
+                    key={`${profile.name}-${index}`}
+                    label={profile.jobTitle}
+                    picture={profile.picture}
+                    title={profile.name}
+                />
+            ))}
         </div>
     );
 }
